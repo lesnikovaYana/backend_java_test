081a09a9e597f7ffb2a811dee68391a4b5393311
lesnikovaYana <lesnikova.yana@yandex.ru>
2021-08-12 17:50:06 +0300

    update GetCategoryTests

--- src/test/java/ru/lesnikovaYana/tests/GetCategoryTest.java
+++ src/test/java/ru/lesnikovaYana/tests/GetCategoryTest.java
@@ -4,7 +4,11 @@ import lombok.SneakyThrows;
 import okhttp3.ResponseBody;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import retrofit2.Response;
 import ru.lesnikovaYana.dto.CategoryResponse;
 import ru.lesnikovaYana.service.CategoryService;
@@ -17,6 +21,7 @@ public class GetCategoryTest extends BaseTest{
 
     @SneakyThrows
     @Test
+    @DisplayName("Запрос данных по валидному ID")
     void getCategoryByIdPositiveTest() {
         Response<CategoryResponse> response = categoryService.getCategory(2).execute();
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
@@ -25,17 +30,21 @@ public class GetCategoryTest extends BaseTest{
     }
 
     @SneakyThrows
-    @Test
-    void getCategoryByIdNonExistentNegativeTest() {
-        Response<CategoryResponse> response = categoryService.getCategory(10).execute();
+    @ParameterizedTest
+    @DisplayName("Запрос данных по несуществующему ID, но в валидном формате")
+    @ValueSource(ints = {10, 45, 14, 7889})
+    void getCategoryByIdNonExistentNegativeTest(int id) {
+        Response<CategoryResponse> response = categoryService.getCategory(id).execute();
         assertThat(response.isSuccessful(), CoreMatchers.is(false));
         assertThat(response.code(), equalTo(404));
     }
 
     @SneakyThrows
-    @Test
-    void getCategoryByIdInvalidNegativeTest() {
-        Response<CategoryResponse> response = categoryService.getCategory(-5).execute();
+    @ParameterizedTest
+    @DisplayName("Запрос данных по невалидному ID")
+    @ValueSource(ints = {-5, -45555, 0, -100, 2147483647})
+    void getCategoryByIdInvalidNegativeTest(int id) {
+        Response<CategoryResponse> response = categoryService.getCategory(id).execute();
         assertThat(response.isSuccessful(), CoreMatchers.is(false));
         assertThat(response.code(), equalTo(404));
     }
